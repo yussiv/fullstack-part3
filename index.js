1555df2af9b07ba09527f97a57e6a14720187455
yussiv <jussi.voipio@cs.helsinki.fi>
2020-05-18 23:26:08 +0300

    3.2

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 const express = require('express')
 
 const app = express()
+const phonebook = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-523523',
+    id: 4
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4
+  },
+]
 
 app.get('/api/persons', (req, res) => {
-  res.json([
-    {
-      name: 'Arto Hellas',
-      number: '040-123456',
-      id: 1
-    },
-    {
-      name: 'Arto Hellas',
-      number: '040-123456',
-      id: 1
-    },
-    {
-      name: 'Ada Lovelace',
-      number: '39-44-523523',
-      id: 4
-    },
-    {
-      name: 'Dan Abramov',
-      number: '12-43-234345',
-      id: 3
-    },
-    {
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122',
-      id: 4
-    },
-  ])
+  res.json(phonebook)
+})
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Phonebook contains ${phonebook.length} entries</p>
+    <p>${new Date()}</p>`)
 })
 
 app.listen(3001, () => {
