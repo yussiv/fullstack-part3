f9dd742b34110c3d751dcbb52cd09b58ccc9a902
yussiv <jussi.voipio@cs.helsinki.fi>
2020-05-19 10:17:33 +0300

    3.7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 let phonebook = [
@@ -25,6 +26,7 @@ let phonebook = [
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
